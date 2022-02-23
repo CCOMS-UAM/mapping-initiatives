5ef1678fd13ccf75c35bbccf92e08f72627cc96b
Daniel Morillo <danivmorillo@gmail.com>
2022-02-23 16:28:32 +0100

    New throughput process integrating the additional spreadsheets provided by Laura and Ellen with extra information to complement the info already published in the repository

--- R/Constants.R
+++ R/Constants.R
@@ -18,39 +18,63 @@ library(glue)
 
 ## ---- CONSTANTS: -------------------------------------------------------------
 
-# Add code here that needs to be run when sourcing this script
 ## File system:
-INITIATIVES_FILENAME <- "synchros-initiatives.xlsx"
-TABLE_1_SHEET        <- "Table 1"
-UPDATED_TABLE_SHEET  <- "Table 1 updated"
-MICA_DATA_FILENAME   <- "mica-export.csv"
 
+### File names:
+INITIATIVES_FILENAME <- "synchros-initiatives.xlsx"
+VAR_CORRESP_FILENAME <- "variable_correspondence.xlsx"
+MICA_DATA_FILENAME   <- "mica-export.csv"
+EXTRAS_FILENAME      <- "PORCENTAJES_template_3_population_.03.07.20.xls"
+UPDATES_FILENAME     <- "SYNCHROS_REPOSITORY_ INITIATIVES_20200617.xlsx"
+
+### File paths:
 DATA_DIR <- "dat"
 INITIATIVES_FILEPATH <- file.path(DATA_DIR, INITIATIVES_FILENAME)
 MICA_DATA_FILEPATH   <- file.path(DATA_DIR, MICA_DATA_FILENAME)
+VAR_CORRESP_FILEPATH <- file.path(DATA_DIR, VAR_CORRESP_FILENAME)
+EXTRAS_FILEPATH      <- file.path(DATA_DIR, EXTRAS_FILENAME)
+UPDATES_FILEPATH     <- file.path(DATA_DIR, UPDATES_FILENAME)
+
+## Excel manipulation:
+TABLE_1_SHEET        <- "Table 1"
+UPDATED_TABLE_SHEET  <- "Table 1 updated"
+COMPLETE_TABLE_SHEET <- "Table 1 complete"
+TABLE_1_EXTRA_SHEET  <- "INITIATIVES"
+TABLE_1_UPDATE_SHEET <- "POPULATION COHORT INITIATIVES"
+
+# Vector of Excel column coordinates (letters), to convert to numeric positions
+EXCEL_COL_POSITIONS <- c(LETTERS, paste0('A', LETTERS))
 
 ## URLs:
 SYNCHROS_URL <- "https://repository.synchros.eu"
 
 ## Text strings:
-EMPTY_STRING               <- ''                              #   in the end
-SPACE                      <- ' '
-COMMA                      <- ', '
-DASH                       <- '-'
-BULLET_PREFIX              <- DASH
-LINE_FEED                  <- "\n"
-COLON                      <- ': '
-SEMICOLON                  <- '; '
-AND_CONJUNCTION            <- " and "
-ASTERISK                   <- '*'
+EMPTY_STRING    <- ''
+SPACE           <- ' '
+COMMA           <- ', '
+DASH            <- '-'
+UNDERSCORE      <- '_'
+PIPE            <- '|'
+BULLET_PREFIX   <- DASH
+SLASH           <- '/'
+LINE_FEED       <- "\n"
+CARRIAGE_RETURN <- "\r"
+COLON           <- ': '
+SEMICOLON       <- '; '
+AND_CONJUNCTION <- " and "
+ASTERISK        <- '*'
+COL_PREFFIX     <- "col_"
 
 ## Regular expressions:
-AUTO_VARNAME_PREFIX_REGEXP <- r"(^\.\.\.)"
-NO_INFO_REGEXP             <- "No information obtained(\\s*)" # Any whitespace
-REPLACE_SEPS_REGEXP        <- glue("({SEMICOLON}|{AND_CONJUNCTION})")
-ENUM_SEPS_REGEXP           <- glue("({COMMA}|{AND_CONJUNCTION})")
-NUMBER_REGEXP              <- r"(\d+)"
-ASTERISK_REGEXP            <- glue("\\{ASTERISK}")
+AUTO_VARNAME_PREFIX_REGEXP     <- r"(^\.\.\.)"
+NO_INFO_REGEXP                 <- "No information obtained(\\s*)"# whitespace(s)
+REPLACE_SEPS_REGEXP            <- glue("({SEMICOLON}|{AND_CONJUNCTION})")
+ENUM_SEPS_REGEXP               <- glue("({COMMA}|{AND_CONJUNCTION})")
+NUMBER_REGEXP                  <- r"(\d+)"
+ASTERISK_REGEXP                <- glue("\\{ASTERISK}")
+UPPERCASE_LETTERS_BEGIN_REGEXP <- "^[A-Z]+"
+UPDATE_VARS_CHARCLASS_REGEXP   <- glue("[{SPACE}{SLASH}{DASH}]")
+ANALYSES_SUFFIX_REGEXP         <- glue("[{SPACE}{DASH}]analyses")
 
 ## Country processing objects:
 VECTOR_CONTINENTS <- codelist |>
