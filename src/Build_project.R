98556276d79a6fce11b4bbbd46bec59a7d5d017b
Daniel Morillo <danivmorillo@gmail.com>
2022-02-24 19:10:37 +0100

    Adds the table written/read checks

--- src/Build_project.R
+++ src/Build_project.R
@@ -20,8 +20,10 @@ rm(list = ls())
 ## ---- INCLUDES: --------------------------------------------------------------
 
 library(rmarkdown)
+library(readxl)
+library(tidyverse)
 
-# source("R/{source_file}", encoding = 'UTF-8')
+source("R/Constants.R", encoding = 'UTF-8')
 
 
 ## ---- CONSTANTS: -------------------------------------------------------------
@@ -35,6 +37,8 @@ WRITE_TABLES <- TRUE # This can be changed to `FALSE` if ones doesn't want
 
 ## ---- MAIN: ------------------------------------------------------------------
 
+# Write notebooks: ----
+
 # Notebook that generates the first version of the updated table:
 render(
   "notebooks/Update_initiatives.Rmd",
@@ -46,3 +50,24 @@ render(
   "notebooks/Complete_initiatives_additional.Rmd",
   params = list(write_file = WRITE_TABLES)
 )
+
+
+# File verification: ----
+
+# Runs the notebook in the current sesson without writng the Excel file:
+source("notebooks/Complete_initiatives_additional.Rmd", echo=TRUE)
+
+read_table <- read_excel(INITIATIVES_FILEPATH, sheet = COMPLETE_TABLE_SHEET)
+
+# This checks that the table written in the excel file and the result after
+#   reading it are coincident:
+initiatives_complete |> select(-id) |> iwalk(
+  ~{
+    comp <- initiatives_complete |> select(id, old = !!sym(.y))  |>
+      full_join(read_table |> select(id, new = !!sym(.y))) |>
+      mutate(eq = old == new)
+    comp |> count(eq) |> print()
+    comp |> filter(is.na(eq)) |> count(old, new) |> print()
+  }
+)
+
