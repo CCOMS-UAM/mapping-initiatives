8f7b9ee915ba2569a8e239a75141dcd495b63ed0
Daniel Morillo <danivmorillo@gmail.com>
2022-02-24 18:34:23 +0100

    Draft of script to build the complete project

---
+++ src/Build_project.R
@@ -0,0 +1,3 @@
+
+rmarkdown::render("~/Workspace/mapping-initiatives/notebooks/Update_initiatives.Rmd", params = list(write_file = TRUE))
+rmarkdown::render("~/Workspace/mapping-initiatives/notebooks/Complete_initiatives_additional.Rmd", params = list(write_file = TRUE))
